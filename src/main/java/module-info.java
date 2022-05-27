427c32141662dc4a5c589eb2cb02d772349a01e1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-05-26 20:22:34 -0400

    added support for JDBC

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
-module ca.georgiancollege.comp1011m2022ice3 {
+module ca.georgiancollege.comp1011m2022ice3
+{
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens ca.georgiancollege.comp1011m2022ice3 to javafx.fxml;
